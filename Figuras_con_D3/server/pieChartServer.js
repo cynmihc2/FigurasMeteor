f0d0e71cf80501c0f2f146197b5feaca417fc42e
Cyntia <cyntia@cyntia-X45U.(none)>
2015-07-17 18:33:25 -0500

    Se sube el archivo pieChart

---
+++ Figuras_con_D3/server/pieChartServer.js
@@ -0,0 +1,25 @@
+Meteor.methods({
+	
+	realizarConsultaPie: function(valor){
+	  
+      var consulta = [ { $group: { _id: "$fill", value:{$sum: 1} } }];
+      var consulta2 = [{$group: { _id:"$fill", figuras: { $push: "$obj_type"}}}];
+      var consulta3 = [{$group: {_id: "$obj_type", figuras: { $push: "$username" }}}];
+      var resultado = Figuras.aggregate(consulta3);
+      console.log("Valor de Resultado: "+resultado);
+	  console.log("Realizó consulta");
+
+	  for(i=0; i<resultado.length; i++){
+  	  var longInternalArray = resultado[i].figuras.length;
+  	  // BUSCAR COMO CAMBIAR figuras POR UN PARÁMETRO 
+  	  //for(j=0; j<longInternalArray; j++){
+  		Slices.insert({"_id": resultado[i]._id, "value": longInternalArray});
+  	  //}
+      }
+	}
+
+});
+
+// Figuras.aggregate([ { $group: { _id: "$fill", value:{$sum: 1} } }, {$out: Bars}]);
+
+    //Figuras.insert({value: "Juan Manuel"});
